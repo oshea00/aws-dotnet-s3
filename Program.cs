81d6705660e58ed45d3b3ab6870b5601e41e5c52
moshea <oshea00@gmail.com>
2020-12-30 00:14:44 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
+
+namespace S3CreateAndList
+{
+    class Program
+    {
+        static Logger log = new LoggerConfiguration()
+                                .WriteTo.Console()
+                                .CreateLogger();
+
+        static IConfigurationRoot configuration =
+            new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile($"appsettings.json")
+                .Build();
+
+        static void Main(string[] args)
+        {
+            try {
+
+                var awsOptions = configuration.GetAWSOptions();
+
+                using (var s3Client = awsOptions.CreateServiceClient<IAmazonS3>()) {
+
+                    var bucketName = "s3create-program-example-73145";
+
+                    var bucketResp = s3Client.PutBucketAsync(
+                        new PutBucketRequest{
+                            BucketName = bucketName,
+                            BucketRegionName = "us-west-2"
+                        } 
+                    ).Result;
+
+                    log.Information("{@BucketResponse}",bucketResp);
+
+                    if (bucketResp.HttpStatusCode == HttpStatusCode.OK) {
+                        log.Information("Bucket Created!");
+                    }
+
+                    foreach (var b in s3Client.ListBucketsAsync().Result.Buckets) {
+                        System.Console.WriteLine(b.BucketName);
+                    }
+
+                    var delResp = s3Client.DeleteBucketAsync(bucketName).Result;
+
+                    log.Information("Done!");
+                }
+
+            } catch (AmazonS3Exception ex) {
+                log.Error($"{ex.ErrorCode}");
+            }
+       }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# AWS dotnet SDK - S3
+Demonstrates the use of AWS dotnet SDK with S3 service.
+
+## Notes
+
+* ```AWS AWSSDK.Extensions.NETCore.Setup``` nuget package.
+  - Provides extensions to the IConfiguration useful for integrating with dotnet config options.
+* Create Bucket, List Buckets, Delete Bucket
+* [AWS SDK docs for .Net](https://docs.aws.amazon.com/sdkfornet/v3/apidocs/index.html)
+
